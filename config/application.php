369d9779786811e3f0384e59eae22d248baf63a1
Vinicius Arcanjo <vinicius@arcanjo.dev>
2022-02-28 06:44:44 -0300

    Added tmp folder

--- config/application.php
+++ config/application.php
@@ -104,6 +104,8 @@ Config::define('DISALLOW_FILE_EDIT', true);
 Config::define('DISALLOW_FILE_MODS', true);
 // Limit the number of post revisions that Wordpress stores (true (default WP): store every revision)
 Config::define('WP_POST_REVISIONS', env('WP_POST_REVISIONS') ?: true);
+// Tmp
+Config::define('WP_TEMP_DIR', dirname(__FILE__) . '/web/app/tmp');
 
 /**
  * S3 Settings
